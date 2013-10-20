698aa52e0739ca98871cfdc758c6a5dd02bead18
jeremyfelt <jeremy.felt@gmail.com>
2013-10-20 13:44:11 -0700

    Enqueue the parent and fonts stylesheets rather than import

--- content/themes/snap_child/functions.php
+++ content/themes/snap_child/functions.php
@@ -23,3 +23,13 @@ add_action( 'after_setup_theme', 'jf_support_post_formats', 99 );
 function jf_support_post_formats() {
 	add_theme_support( 'post-formats', array( 'aside', 'image', 'gallery', 'link', 'quote', 'status', 'video', 'audio', 'chat' ) );
 }
+
+add_action( 'wp_enqueue_scripts', 'jf_enqueue_styles', 9 );
+/**
+ * Enqueue the parent theme style and custom Google fonts before the child theme
+ * stylesheet is loaded.
+ */
+function jf_enqueue_styles() {
+	wp_enqueue_style( 'jf-snap-fonts', 'http://fonts.googleapis.com/css?family=News+Cycle:400,700|Oxygen:400,700|Raleway:400,700' );
+	wp_enqueue_style( 'snap-parent-style', get_template_directory_uri() . '/style.css' );
+}
