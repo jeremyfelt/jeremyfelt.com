35fc1e9eb6df0e987e773f91dc913502c26026b0
jeremyfelt <jeremy.felt@gmail.com>
2013-10-19 18:22:47 -0700

    Add support for post formats in the Snap child theme

--- content/themes/snap_child/functions.php
+++ content/themes/snap_child/functions.php
@@ -13,3 +13,13 @@ add_filter( 'jetpack_enable_opengraph', 'jf_disable_jetpack_opengraph', 999 );
 function jf_disable_jetpack_opengraph() {
 	return false;
 }
+
+add_action( 'after_setup_theme', 'jf_support_post_formats', 99 );
+/**
+ * Snap doesn't support post formats by default, but I'd still like to track them
+ * when I create posts as part of a longer term strategy to have some kind of
+ * unique display or handling for each.
+ */
+function jf_support_post_formats() {
+	add_theme_support( 'post-formats', array( 'aside', 'image', 'gallery', 'link', 'quote', 'status', 'video', 'audio', 'chat' ) );
+}
