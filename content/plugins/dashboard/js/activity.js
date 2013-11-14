2a8464de02c646dbe21420dd833fe41889597da9
jeremyfelt <jeremy.felt@gmail.com>
2013-11-13 21:02:38 -0800

    Remove feature plugin Dash, now in 3.8 trunk

--- content/plugins/dashboard/js/activity.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-	$(document).ready( function() {
-		$( '.show-more a' ).on( 'click', function(e) {
-			$( this ).fadeOut().closest('.activity-block').find( 'li.hidden' ).fadeIn().removeClass( 'hidden' );
-			e.preventDefault();
-		})
-	});
-}(jQuery));
