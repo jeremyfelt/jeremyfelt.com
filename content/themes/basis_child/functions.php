4e2c1e504ada005adbaa1090d6e1d638f17ef213
jeremyfelt <jeremy.felt@gmail.com>
2013-12-31 19:22:21 -0800

    Add custom functionality to child theme

--- content/themes/basis_child/functions.php
+++ content/themes/basis_child/functions.php
@@ -2,3 +2,24 @@
 /**
  * @package Basis Child
  */
+
+add_filter( 'jetpack_enable_open_graph', 'jf_disable_jetpack_opengraph', 999 );
+add_filter( 'jetpack_enable_opengraph', 'jf_disable_jetpack_opengraph', 999 );
+/**
+ * Jetpack does a blind output of opengraph data, which conflicts with Yoast SEO
+ *
+ * @return bool false to ensure that Jetpack does not try to output opengraph tagging
+ */
+function jf_disable_jetpack_opengraph() {
+	return false;
+}
+
+add_action( 'after_setup_theme', 'jf_support_post_formats', 99 );
+/**
+ * Snap doesn't support post formats by default, but I'd still like to track them
+ * when I create posts as part of a longer term strategy to have some kind of
+ * unique display or handling for each.
+ */
+function jf_support_post_formats() {
+	add_theme_support( 'post-formats', array( 'aside', 'image', 'gallery', 'link', 'quote', 'status', 'video', 'audio', 'chat' ) );
+}
