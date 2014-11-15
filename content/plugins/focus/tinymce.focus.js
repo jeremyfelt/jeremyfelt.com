5baa3196342ad2f9fadcfb92506274bd097eb543
jeremyfelt <jeremy.felt@gmail.com>
2014-11-14 18:44:25 -0800

    Remove Focus plugin now that it is in WP core

--- content/plugins/focus/tinymce.focus.js
+++
@@ -1,7 +0,0 @@
-( function( mce, $ ) {
-	'use strict';
-
-	mce.PluginManager.add( 'focus', function( editor ) {
-		$( document ).triggerHandler( 'tinymce-editor-setup', [ editor ] );
-	} );
-} )( window.tinymce, window.jQuery );
