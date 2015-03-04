eef955f461784c8cbebf3e755a2765b4c58fe9cc
jeremyfelt <jeremy.felt@gmail.com>
2015-03-03 19:55:06 -0800

    Remove Make theme

--- content/themes/make/partials/entry-meta-before-content.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @package Make
- */
-global $ttfmake_current_location;
-$ttfmake_current_location = 'before-content';
-get_template_part( 'partials/entry', 'meta' );
-unset( $ttfmake_current_location );
--- content/themes/make/partials/entry-meta-top.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @package Make
- */
-global $ttfmake_current_location;
-$ttfmake_current_location = 'top';
-get_template_part( 'partials/entry', 'meta' );
-unset( $ttfmake_current_location );
