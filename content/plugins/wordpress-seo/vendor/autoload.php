b45bca2bace917169f87f59ff4006419cad37305
Jeremy Felt <jeremy.felt@gmail.com>
2016-03-12 19:44:20 -0800

    Update WordPress SEO

--- content/plugins/wordpress-seo/vendor/autoload.php
+++ content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit128865ac84ba1269f80cb4dabfc80f34::getLoader();
+return ComposerAutoloaderInitf4a76b54f454685d869d012b4e7f9b27::getLoader();
--- content/plugins/wordpress-seo/vendor/autoload_52.php
+++ content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit27ff46d128d551a41350e87784a0143e::getLoader();
+return ComposerAutoloaderInit4d373c99c96854889c296a41d0bfda82::getLoader();
