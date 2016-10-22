85ee383bc252edf40ad98022eca58daede1bb9be
Jeremy Felt <jeremy.felt@gmail.com>
2016-10-22 13:02:21 -0700

    Add Google Analytics to mu plugin

--- content/mu-plugins/jf-analytics.php
+++ content/mu-plugins/jf-analytics.php
@@ -24,3 +24,20 @@ function jf_head() {
 	<meta name="google-site-verification" content="rPLtSyhW-8YGAT9US8ogGbuLnSPzOfoXfZar6n6t1zA" />
 	<?php
 }
+
+add_action( 'wp_head', 'jf_ga_head', 999 );
+function jf_ga_head() {
+	?>
+	<!-- Google Analytics -->
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-39564638-1', 'auto');
+	ga('send', 'pageview');
+	</script>
+	<!-- End Google Analytics -->
+	<?php
+}
