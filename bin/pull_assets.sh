b9b324c755ac7e8e5dfb529d1b620c505f372154
Jeremy Felt <jeremy.felt@gmail.com>
2019-03-22 18:02:38 -0700

    Add script to sync assets from production

---
+++ bin/pull_assets.sh
@@ -0,0 +1,2 @@
+rsync -rvzh -e ssh --delete --exclude '*.git*' foghlaimeoir:/var/www/jeremyfelt.com/content/uploads/ wp-content/uploads/
+rsync -rvzh -e ssh --delete --exclude '*.git*' foghlaimeoir:/var/www/jeremyfelt.com/content/images/ wp-content/images/
