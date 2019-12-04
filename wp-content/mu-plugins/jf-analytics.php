7db72c88857fde8d06c410c5b11c660823c9ecfe
Jeremy Felt <jeremy.felt@gmail.com>
2019-12-03 18:43:16 -0800

    Remove old HEAD links and add Matomo script

--- wp-content/mu-plugins/jf-analytics.php
+++ wp-content/mu-plugins/jf-analytics.php
@@ -3,41 +3,25 @@
  * Various tags and things
  */
 
-add_action( 'wp_head', 'jf_head', 10 );
-function jf_head() {
-	// Add the tag provided by analytics.twitter.com, mostly to find out why.
-	?>
-	<meta property="twitter:account_id" content="1741681" /><?php
-
-	// Add the tag that connects me to Google+
-	?>
-	<link rel="me" type="text/html" href="http://www.google.com/profiles/jeremy.felt"/>
-	<?php
-
-	// Add the tag that validates with Microsoft!
-	?>
-	<meta name="msvalidate.01" content="7F45AEBECE60DA943AD0E712BD3BDBD6" />
-	<?php
-
-	// Add the tag that validates with Google Webmaster Tools
-	?>
-	<meta name="google-site-verification" content="rPLtSyhW-8YGAT9US8ogGbuLnSPzOfoXfZar6n6t1zA" />
-	<?php
-}
-
 add_action( 'wp_head', 'jf_ga_head', 999 );
 function jf_ga_head() {
 	?>
-	<!-- Google Analytics -->
-	<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-	ga('create', 'UA-39564638-1', 'auto');
-	ga('send', 'pageview');
+	<!-- Site traffic tracking through a self-hosted instance of Matomo. IP addresses
+         are "anonymized" to 2 octets so that 192.168.123.345 becomes 192.168.0.0 in
+	     my dashboard view.
+	-->
+	<script type="text/javascript">
+	var _paq = window._paq || [];
+	/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+	_paq.push(['trackPageView']);
+	_paq.push(['enableLinkTracking']);
+	(function() {
+		var u="//jeremyfelt.com/";
+		_paq.push(['setTrackerUrl', u+'matomo.php']);
+		_paq.push(['setSiteId', '1']);
+		var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+		g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+	})();
 	</script>
-	<!-- End Google Analytics -->
 	<?php
 }
