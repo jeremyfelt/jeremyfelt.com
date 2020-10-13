50e6896b48aa1e465b676ee8cd5cdb10a00402f1
Jeremy Felt <jeremy.felt@gmail.com>
2020-10-12 17:28:13 -0700

    Update that wp-content directory once again

--- bin/pull_plugins.sh
+++ bin/pull_plugins.sh
@@ -1,1 +1,1 @@
-rsync -rvzh -e ssh --delete --exclude-from=.deploy_ignore foghlaimeoir:/var/www/jeremyfelt.com/www/wordpress/wp-content/plugins/ wp-content/plugins/
+rsync -rvzh -e ssh --delete --exclude-from=.deploy_ignore foghlaimeoir:/var/www/jeremyfelt.com/wp-content/plugins/ wp-content/plugins/
