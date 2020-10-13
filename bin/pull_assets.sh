25fed08b909f1f736357bec69d75982259351b04
Jeremy Felt <jeremy.felt@gmail.com>
2020-10-12 17:01:46 -0700

    Update wp-content path on remote

--- bin/pull_assets.sh
+++ bin/pull_assets.sh
@@ -1,2 +1,2 @@
-rsync -rvzh -e ssh --delete --exclude '*.git*' foghlaimeoir:/var/www/jeremyfelt.com/www/wp-content/uploads/ wp-content/uploads/
-rsync -rvzh -e ssh --delete --exclude '*.git*' foghlaimeoir:/var/www/jeremyfelt.com/www/wp-content/images/ wp-content/images/
+rsync -rvzh -e ssh --delete --exclude '*.git*' foghlaimeoir:/var/www/jeremyfelt.com/www/wordpress/wp-content/uploads/ wp-content/uploads/
+rsync -rvzh -e ssh --delete --exclude '*.git*' foghlaimeoir:/var/www/jeremyfelt.com/www/wordpress/wp-content/images/ wp-content/images/
