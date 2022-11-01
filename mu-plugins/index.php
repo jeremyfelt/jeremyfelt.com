3efc70f077cb38c7644a320f596f29f465310793
Jeremy Felt <jeremy.felt@gmail.com>
2022-10-31 22:42:55 -0700

    Filter Share on Mastodon to output note content

--- mu-plugins/index.php
+++ mu-plugins/index.php
@@ -12,3 +12,4 @@ require_once __DIR__ . '/base/likes.php';
 require_once __DIR__ . '/plugins/content-visibility.php';
 require_once __DIR__ . '/plugins/indieweb.php';
 require_once __DIR__ . '/plugins/micropub.php';
+require_once __DIR__ . '/plugins/share-on-mastodon.php';
---
+++ mu-plugins/plugins/share-on-mastodon.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace JeremyFelt\Plugins\ShareOnMastodon;
+
+add_filter( 'share_on_mastodon_status', __NAMESPACE__ . '\filter_status_text', 10, 2 );
+
+/**
+ * Filter a toot so that the note content is used rather than the title.
+ *
+ * @param string   $status The status text.
+ * @param \WP_Post $post   The post object.
+ * @return string The modified status text.
+ */
+function filter_status_text( $status, $post ) {
+	$status = apply_filters( 'the_content', $post->post_content );
+
+	// Do what the plugin does to the title, but to the rendered content.
+	$status = wp_strip_all_tags(
+		html_entity_decode( $status, ENT_QUOTES | ENT_HTML5, get_bloginfo( 'charset' ) ) // Avoid double-encoded HTML entities.
+	);
+
+	// Append the URL, for now.
+	$status .= ' ' . esc_url_raw( get_permalink( $post->ID ) );
+
+	return $status;
+}
