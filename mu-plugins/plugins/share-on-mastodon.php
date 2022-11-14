97354709d821fe3c958bace7786e396294ad6365
Jeremy Felt <jeremy.felt@gmail.com>
2022-11-14 06:43:12 -0800

    Convert anchors in post text to markdown for Mastodon

--- mu-plugins/plugins/share-on-mastodon.php
+++ mu-plugins/plugins/share-on-mastodon.php
@@ -13,6 +13,7 @@ add_filter( 'share_on_mastodon_status', __NAMESPACE__ . '\filter_status_text', 1
  */
 function filter_status_text( $status, $post ) {
 	$status = apply_filters( 'the_content', $post->post_content );
+	$status = convert_to_markdown( $status );
 
 	// Do what the plugin does to the title, but to the rendered content.
 	$status = wp_strip_all_tags(
@@ -20,4 +21,33 @@ function filter_status_text( $status, $post ) {
 	);
 
 	return $status;
+}
+
+/**
+ * Convert a basic set of HTML tags into their markdown equivalent.
+ *
+ * @param string $html The post content.
+ * @return string Modified post content.
+ */
+function convert_to_markdown( string $html ) : string {
+	$document = new \DOMDocument();
+	@$document->loadHTML( $html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD ); // phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged
+
+	$anchors = $document->getElementsByTagName( 'a' );
+	$count   = $anchors->length - 1;
+
+	for ( $count; $count > -1; $count-- ) {
+		$node = $anchors->item( $count );
+
+		$link_text = $node->nodeValue; // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+		$link_href = $node->getAttribute( 'href' );
+
+		if ( '' !== $link_href ) {
+			$text_node = $document->createTextNode( '[' . $link_text . '](' . $link_href . ')' );
+
+			$node->parentNode->replaceChild( $text_node, $node ); // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+		}
+	}
+
+	return $document->saveHTML();
 }
