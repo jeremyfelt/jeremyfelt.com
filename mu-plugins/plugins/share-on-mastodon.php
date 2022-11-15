e5f0f7073eea4b66f7bfba6bf5253746e892704f
Jeremy Felt <jeremy.felt@gmail.com>
2022-11-14 20:29:14 -0800

    Add initial support for replies to toots via notes

--- mu-plugins/plugins/share-on-mastodon.php
+++ mu-plugins/plugins/share-on-mastodon.php
@@ -5,6 +5,43 @@ namespace JeremyFelt\Plugins\ShareOnMastodon;
 add_filter( 'share_on_mastodon_status', __NAMESPACE__ . '\filter_status_text', 10, 2 );
 
 /**
+ * Filter Mastodon toot args to include a reply to ID if it exists.
+ *
+ * @param array    $args The args sent with a new Mastodon post.
+ * @param \WP_Post $post The post object.
+ */
+function filter_args( array $args, \WP_Post $post ) : array {
+	if ( 'shortnote' !== $post->post_type ) {
+		return $args;
+	}
+
+	$reply_to_url = get_post_meta( $post->ID, 'shortnotes_reply_to_url', true );
+
+	if ( ! $reply_to_url ) {
+		return $args;
+	}
+
+	$reply_host = wp_parse_url( $reply_to_url, PHP_URL_HOST );
+	$mastodon   = get_user_meta( $post->post_author, 'mastodon', true );
+
+	if ( ! $mastodon || $reply_host !== wp_parse_url( $mastodon, PHP_URL_HOST ) ) {
+		return $args;
+	}
+
+	// https://mastodon.host/web/@user@users.host/{in_rely_to_id}
+	$path = trim( wp_parse_url( $reply_to_url, PHP_URL_PATH ), '/' );
+	$path = explode( '/', $path );
+
+	if ( 3 !== count( $path ) || 'web' !== $path[0] || ! is_numeric( $path[2] ) ) {
+		return $args;
+	}
+
+	$args['in_reply_to_id'] = $path[2];
+
+	return $args;
+}
+
+/**
  * Filter a toot so that the note content is used rather than the title.
  *
  * @param string   $status The status text.
@@ -18,6 +55,13 @@ function filter_status_text( $status, $post ) {
 	// Do what the plugin does to the title, but to the rendered content.
 	$status = wp_strip_all_tags(
 		html_entity_decode( $status, ENT_QUOTES | ENT_HTML5, get_bloginfo( 'charset' ) ) // Avoid double-encoded HTML entities.
+	);
+
+	add_filter(
+		'share_on_mastodon_toot_args',
+		function( $args ) use ( $post ) {
+			return filter_args( $args, $post );
+		}
 	);
 
 	return $status;
