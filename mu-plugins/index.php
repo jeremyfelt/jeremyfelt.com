b5d63a8d8dffd63e5674b867a1e6628109df28c5
Jeremy Felt <jeremy.felt@gmail.com>
2024-01-10 21:20:24 -0800

    Redirect author archives to home page
    Fixes #39

--- mu-plugins/index.php
+++ mu-plugins/index.php
@@ -6,6 +6,7 @@
 require_once __DIR__ . '/wp/emoji.php';
 require_once __DIR__ . '/wp/head.php';
 require_once __DIR__ . '/wp/styles.php';
+require_once __DIR__ . '/wp/urls.php';
 
 require_once __DIR__ . '/plugins/content-visibility.php';
 require_once __DIR__ . '/plugins/indieweb.php';
